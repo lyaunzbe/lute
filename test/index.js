d2bdba3da1150bdeecdf65c3486617815900de79
Ben L <lyaunzon.b@gmail.com>
2013-02-19 02:14:43 -0500

    adding support for wildcard transcodes, multiple output files

--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
 var test  = require('tap').test,
-    phaser  = require('./../tasks/phaser.json'),
     lute  = require('./../lib/lute');
 
 
@@ -13,22 +12,50 @@ test('Module definition', function(t){
   t.end();
 });
 
+var phaser = {
+  "effects":{
+    "phaser": "0.6 0.66 3.0 0.6 2.0 -t"
+  }
+};
+
 /**
  * Add a simple phaser effect onto two .ogg files.
- * Ensure no errors
+ * No filenames given. Ensure no errors
  */
-test('Processesing an example task', function(t){
+test('Processesing a basic task', function(t){
   t.plan(1);
   lute(['rick.ogg', 'showtime.ogg'], phaser, function(err){
     t.equal(null, err, 'Should return no errors or warnings');
+    //Add test that actually checks that the new files exist.
   });
 });
+
+var transcode1 = {
+  "outfile":{
+    "filename": "*.mp3"
+  }
+};
+
+/**
+ * Batch transcode, wildcard output filename given.
+ * Ensure no errors.
+ */
+
+var transcode2 = {
+  "outfile":{
+    "filename": ['ricky.mp3', 'show.wav' ]
+  }
+};
+
+/**
+ * Batch transcode, filenames provided for each.
+ * Ensure no errors.
+ */
 
 /**
  * A more complicated effect chain, using format options for
  * the output file. Ensure no errors.
  */
-
 
 /**
  * Example of batch transcode, no output filenames provided, 
